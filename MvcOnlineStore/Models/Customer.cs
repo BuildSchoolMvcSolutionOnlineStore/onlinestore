63f2ea4f50fdf203d87159024bbf38262661d140
立心 劉 <swes910122@gmail.com>
2018-05-09 18:40:02 +0800

    新建 CustomerRepository 專案 和 CustomerRepository 類別 與 新增 修改 刪除 查詢 方法

--- MvcOnlineStore/Models/Customer.cs
+++ MvcOnlineStore/Models/Customer.cs
@@ -8,6 +8,12 @@ namespace BuildSchool.MvcSolution.OnlineStore.Models
 {
     public class Customer
     {
-
+        public int CustomerID { get; set; }
+        public string CustomerAccountNumber { get; set; }
+        public string CustomerPassword { get; set; }
+        public string CustomerName { get; set; }
+        public int Telephone { get; set; }
+        public string Address { get; set; }
+        public string CustomerMail { get; set; }
     }
 }
