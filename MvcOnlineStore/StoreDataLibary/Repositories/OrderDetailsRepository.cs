e5283784aa7e42739f6b2c4e847a76e3a6bb7ff6
PeterHu <PeterHu@PETERHU>
2018-05-10 15:54:53 +0800

    修改OrderDetailsRepository，OrderDetailsRepository單元測試

--- MvcOnlineStore/StoreDataLibary/Repositories/OrderDetailsRepository.cs
+++ MvcOnlineStore/StoreDataLibary/Repositories/OrderDetailsRepository.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace BuildSchool.MvcSolution.OnlineStore.Models.Repositories
 {
-    class OrderDetailsRepository
+    public class OrderDetailsRepository
     {
         string serviceIP = "192.168.40.21";
         public void Create(OrderDetails model)
@@ -67,9 +67,7 @@ namespace BuildSchool.MvcSolution.OnlineStore.Models.Repositories
 
             command.Parameters.AddWithValue("@id", orderId);
 
-            var result = command.ExecuteScalar();//純量值
-            //如果查詢資料是NULL的話
-            //if (result == DBNull.Value)
+            
             connection.Open();
 
             var reader = command.ExecuteReader(CommandBehavior.CloseConnection);
@@ -114,6 +112,7 @@ namespace BuildSchool.MvcSolution.OnlineStore.Models.Repositories
                 orderDetail.ProductID = reader.GetValue(reader.GetOrdinal("ProductID")).ToString();
                 orderDetail.Quantity = Convert.ToInt32(reader.GetValue(reader.GetOrdinal("Quantity")));
                 orderDetail.Discount = Convert.ToInt32(reader.GetValue(reader.GetOrdinal("Discount")));
+                orderDetails.Add(orderDetail);
             }
 
             reader.Close();
