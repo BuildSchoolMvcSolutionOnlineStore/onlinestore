acf043929f3496debc3628bd524e280fc7618c64
PeterHu <PeterHu@PETERHU>
2018-05-10 11:03:42 +0800

    新增OrdereDetails Model

---
+++ MvcOnlineStore/StoreDataLibary/Models/OrderDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace BuildSchool.MvcSolution.OnlineStore.Models.Models
+{
+    class OrderDetails
+    {
+        public string OrderID { get; set; }
+        public string ProductID { get; set; }
+        public int Quantity { get; set; }
+        public int Discount { get; set; }
+    }
+}
