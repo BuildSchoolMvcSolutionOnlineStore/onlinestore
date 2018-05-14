ca8b979f21e51dad578795d5a19e3230ec90ff21
立心 劉 <swes910122@gmail.com>
2018-05-14 12:25:59 +0800

    修改CustomersRepository， 新增 新增刪除修改CustomersRepositoryTests

--- MvcOnlineStore/StoreDataLibary/Repositories/CustomersRepository.cs
+++ MvcOnlineStore/StoreDataLibary/Repositories/CustomersRepository.cs
@@ -10,11 +10,9 @@ namespace BuildSchool.MvcSolution.OnlineStore.Models.Repositories
 {
     public class CustomersRepository
     {
-        string serviceIP = "192.168.40.21";
         public void Create(Customer model)
         {
-            SqlConnection connection = new SqlConnection(
-                "Server=" + serviceIP + ";Database=Shopping;User Id=linker;Password = 19960705;");
+            SqlConnection connection = new SqlConnection(SqlConnectionString.ConnectionString);
             var sql = "INSERT INTO Customers VALUES(@CustomerID,@CustomerPassword,@CustomerName,@Telephone,@Address,@CustomerMail)";
             SqlCommand command = new SqlCommand(sql, connection);
             command.Parameters.AddWithValue("@customerID", model.CustomerID);
@@ -30,64 +28,52 @@ namespace BuildSchool.MvcSolution.OnlineStore.Models.Repositories
         }
         public void Update(Customer model)
         {
-            SqlConnection connection = new SqlConnection(
-                "Server=" + serviceIP + ";Database=Shopping;User Id=linker;Password = 19960705;");
-            var sql = "UPRATE Customers SET @CustomerPassword,@CustomerName,@Telephone,@Address,@CustomerMail WHERE CustomerID = @id";
+            SqlConnection connection = new SqlConnection(SqlConnectionString.ConnectionString);
+            var sql = "UPRATE Customers SET @CustomerPassword=CustomerPassword,@CustomerName=CustomerName,@Telephone=Telephone,@Address=Address,@CustomerMail=CustomerMail WHERE CustomerID = @id";
             SqlCommand command = new SqlCommand(sql, connection);
             command.Parameters.AddWithValue("@id", model.CustomerID);
-            command.Parameters.AddWithValue("@customerPassword", model.CustomerPassword);
-            command.Parameters.AddWithValue("@customerName", model.CustomerName);
-            command.Parameters.AddWithValue("@telephone", model.Telephone);
-            command.Parameters.AddWithValue("@address", model.Address);
+            command.Parameters.AddWithValue("@CustomerPassword", model.CustomerPassword);
+            command.Parameters.AddWithValue("@CustomerName", model.CustomerName);
+            command.Parameters.AddWithValue("@Telephone", model.Telephone);
+            command.Parameters.AddWithValue("@Address", model.Address);
             command.Parameters.AddWithValue("@CustomerMail", model.CustomerMail);
 
             connection.Open();//連線打開
             command.ExecuteNonQuery();//執行指令
             connection.Close();//關閉結束
         }
-        public void Delete(Customer model)
+
+        public void Delete(String CustomerID)
         {
-            SqlConnection connection = new SqlConnection(
-                "Server=" + serviceIP + ";Database=Shopping;User Id=linker;Password = 19960705;");
+            SqlConnection connection = new SqlConnection(SqlConnectionString.ConnectionString);
             var sql = "Delete From Customers WHERE CustomerID = @id";
 
             SqlCommand command = new SqlCommand(sql, connection);
 
-            command.Parameters.AddWithValue("@id", model.CustomerID);
+            command.Parameters.AddWithValue("@id", CustomerID);
             connection.Open();//連線打開
             command.ExecuteNonQuery();//執行指令
             connection.Close();//關閉結束
         }
-        public Customer FindById(string customerId)
+        public Customer FindById(string CustomerId)
         //單筆資料查詢
         {
-            SqlConnection connection = new SqlConnection(
-                "Server=" + serviceIP + ";Database=Shopping;User Id=linker;Password = 19960705;");
+            SqlConnection connection = new SqlConnection(SqlConnectionString.ConnectionString);
             var sql = "SELECT * FROM Customers WHERE CustomerID = @id";
 
             SqlCommand command = new SqlCommand(sql, connection);
 
-            command.Parameters.AddWithValue("@id", customerId);
+            command.Parameters.AddWithValue("@id", CustomerId);
 
             connection.Open();
 
-            var reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+            var reader = command.ExecuteReader();
             var properties = typeof(Customer).GetProperties();
             Customer customer = null;
 
             while (reader.Read())
             {
-                customer = new Customer();
-                for (var i = 0; i < reader.FieldCount; i++)
-                {
-                    var fieldName = reader.GetName(i);
-                    var property = properties.FirstOrDefault(p => p.Name == fieldName);
-                    if(property == null)
-                        continue;
-
-                    if (!reader.IsDBNull(i))
-                        property.SetValue(customer, reader.GetValue(i));
-                }           
+                customer = DbReaderModelBinder<Customer>.Bind(reader); 
             }
 
             reader.Close();
@@ -95,8 +81,7 @@ namespace BuildSchool.MvcSolution.OnlineStore.Models.Repositories
         }
         public IEnumerable<Customer> GetAll()
         {
-            SqlConnection connection = new SqlConnection(
-                "Server=" + serviceIP + ";Database=Shopping;User Id=linker;Password = 19960705;");
+            SqlConnection connection = new SqlConnection(SqlConnectionString.ConnectionString);
 
             var sql = "SELECT * FROM Customers";
 
@@ -110,17 +95,7 @@ namespace BuildSchool.MvcSolution.OnlineStore.Models.Repositories
 
             while (reader.Read())
             {
-                customer = new Customer();
-                for (var i = 0; i < reader.FieldCount; i++)
-                {
-                    var fieldName = reader.GetName(i);
-                    var property = properties.FirstOrDefault(p => p.Name == fieldName);
-                    if (property == null)
-                        continue;
-
-                    if (!reader.IsDBNull(i))
-                        property.SetValue(customer, reader.GetValue(i));
-                }
+                customer = DbReaderModelBinder<Customer>.Bind(reader);
                 customerlist.Add(customer);
             }
 
--- MvcOnlineStore/StoreDataLibaryTests/Repositories/CustomersRepositoryTests.cs
+++ MvcOnlineStore/StoreDataLibaryTests/Repositories/CustomersRepositoryTests.cs
@@ -15,7 +15,7 @@ namespace BuildSchool.MvcSolution.OnlineStore.Models.Repositories.Tests
         public void FindByIdTest()
         {
             var repository = new CustomersRepository();
-            var text = repository.FindById("1");
+            var text = repository.FindById("Alice5278");
             Assert.IsTrue(text != null);
         }
 
@@ -25,6 +25,43 @@ namespace BuildSchool.MvcSolution.OnlineStore.Models.Repositories.Tests
             var repository = new CustomersRepository();
             var list = repository.GetAll();
             Assert.IsTrue(list.Count() > 0);
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            var repository = new CustomersRepository();
+            var model = new Customer();
+            model.CustomerID = "Test";
+            model.CustomerPassword = "1234";
+            model.CustomerName = "Testabc";
+            model.Telephone = 0900000000;
+            model.Address = "CHU";
+            model.CustomerMail = "Testabc@gmail.com";
+            repository.Create(model);
+            var text = repository.FindById("Test");
+            Assert.IsTrue(text != null);
+        }
+
+        [TestMethod()]
+        public void UpdateTest()
+        {
+            var repository = new CustomersRepository();
+            var Oldmodel = repository.FindById("Test");
+            Oldmodel.CustomerName = "客戶修改";
+            repository.Update(Oldmodel);
+
+            var Newmodel = repository.FindById("Test");
+            Assert.IsTrue(Newmodel.CustomerName == "客戶修改");
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            var repository = new CustomersRepository();
+            repository.Delete("Test");
+            var text = repository.FindById("Test");
+            Assert.IsTrue(text == null);
         }
     }
 }
