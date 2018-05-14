8f904bdd2ab7a55d188f0239ef6f39f7aea402d4
PeterHu <PeterHu@PETERHU>
2018-05-14 12:01:15 +0800

    新增PaymentMethodsRepositoryTests

---
+++ MvcOnlineStore/StoreDataLibaryTests/Repositories/PaymentMethodsRepositoryTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BuildSchool.MvcSolution.OnlineStore.Models.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BuildSchool.MvcSolution.OnlineStore.Models.Models;
+
+namespace BuildSchool.MvcSolution.OnlineStore.Models.Repositories.Tests
+{
+    [TestClass()]
+    public class PaymentMethodsRepositoryTests
+    {
+        [TestMethod()]
+        public void CreatePaymentMethodsTest()
+        {
+            var repository = new PaymentMethodsRepository();
+            var model = new PaymentMethods();
+            model.PaymentMethodID = 5;
+            model.PaymentMethod = "付款方法測試";
+            repository.CreatePaymentMethods(model);
+            var text = repository.FindPaymentMethodsByPaymentMethodsId(5);
+            Assert.IsTrue(text != null);
+        }
+    }
+}
