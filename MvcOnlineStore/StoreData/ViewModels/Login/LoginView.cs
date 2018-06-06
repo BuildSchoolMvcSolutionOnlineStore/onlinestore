dd8701a23ed26de611e54dd8e9d1d2eb6f07dc33
alan tsai <zeroalan86@gmail.com>
2018-06-06 16:48:30 +0800

    新增後台Login

---
+++ MvcOnlineStore/StoreData/ViewModels/Login/LoginView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StoreData.ViewModels.Login
+{
+    public class LoginView
+    {
+        [DisplayName("員工編號")]
+        [Required(ErrorMessage = "請輸入員工編號")]
+        public string ManagerID { get; set; }
+
+        [DisplayName("密碼")]
+        [Required(ErrorMessage = "請輸入密碼")]
+        public string ManagerPassword { get; set; }
+    }
+}
