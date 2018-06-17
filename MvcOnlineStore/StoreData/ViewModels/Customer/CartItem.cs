9a8b257f73e1ba3c4785f429288f75f86bf46b3b
升偉 栢 <a0975922653@gmail.com>
2018-06-18 01:43:37 +0800

    還沒把購物車加入資料庫，可取得資料庫原有的購物車資料

--- MvcOnlineStore/StoreData/ViewModels/Customer/CartItem.cs
+++ MvcOnlineStore/StoreData/ViewModels/Customer/CartItem.cs
@@ -1,13 +1,14 @@
-﻿using StoreData.ViewModels.Home;
+﻿using StoreData.Models;
+using StoreData.ViewModels.Home;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Helpers;
+
 
 namespace StoreData.ViewModels.Customer
 {
-    public class ChartItem
+    public class CartItem
     {
         public string CustomerID { get; set; }
         public string ProductID { get; set; }
@@ -15,7 +16,7 @@ namespace StoreData.ViewModels.Customer
         public int UnitPrice { get; set; }
         public string Path { get; set; }
         public string ProductName { get; set; }
-        public IEnumerable<ChartItem> GetAllChartList { get; set; }
+        public IEnumerable<Cart> GetAllChartList { get; set; }
 
     }
 }
